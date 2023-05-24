302b39110db907827bfb9fdee994aada0a42d4ec
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-05-24 17:56:32 -0300

    Projeto Bloqueador Tik Tok Toe

---
+++ bloqueador_tik_tok_toe.py
@@ -0,0 +1,15 @@
+'''Bloqueador Tic Tac Toe: Neste desafio do Python, escreva uma função 
+que aceite dois números. Esses números representarão uma posição 
+em um tabuleiro de jogo da velha. A função deve retornar o número do ponto
+que pode impedir que esses dois pontos ganhem o jogo.'''
+
+print('====VAMOS JOGAR O JOGO DA VELHA===')
+print('Você vai precisar informar dois números entre 1 e 9.')
+
+n1 = input('Informe o primeiro número: ')
+print(n1)
+
+n2 = input('Informe o segundo número, e iniciaremos o jogo: ')
+print(n2)
+
+print('FIM!!!')
