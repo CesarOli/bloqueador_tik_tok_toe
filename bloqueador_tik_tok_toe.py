978ac51c01938fb997cb9857b084313f0e2ca60b
César Oliveira <73895379+CesarOli@users.noreply.github.com>
2023-05-25 16:35:54 -0300

    inclusão da função randint(escolha aleatório de numero feita pela máquina).

--- bloqueador_tik_tok_toe.py
+++ bloqueador_tik_tok_toe.py
@@ -1,10 +1,13 @@
+from random import randint
+
 print('====VAMOS JOGAR O JOGO DA VELHA===')
 print('Você vai precisar informar dois números entre 1 e 9.')
 
 n1 = input('Informe o primeiro número: ')
 print(n1)
 
-n2 = input('Informe o segundo número, e iniciaremos o jogo: ')
+n2 = randint(1,9)
+print('O número escolhido pelo computador foi: ',+ n2)
 print(n2)
 
 print('FIM!!!')
